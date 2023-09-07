1879ddbbda8babe9bdc978c0e2f8fc29c65f1fad
abdul <abdulhakim1699rabbit@gmail.com>
2023-09-07 22:28:43 +0800

    Header 1

--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,10 +1,36 @@
 import React from 'react'
 import './header.css';
+import people from '../../assets/people.png';
+import ai from '../../assets/ai.png';
 
 const Header = () => {
   return (
-    <div>
-      Header
+    <div className="gpt3__header section__padding" id="home">
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Something amazing with GPT-3 OpenAI
+        </h1>
+        <p>
+          GPT-3 is a new language model that’s been trained on the largest publicly available dataset of English text, covering 40GB of trials and questions
+        </p>
+
+        <div className="gpt3__header-content__input">
+        <input type="email" placeholder="Enter your email address"/>
+        <button type="button">Get Started</button> 
+        </div>
+
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people"/>
+          <p>
+            1,600 people requested access a visit in the last 24 hours
+          </p>
+        </div>
+
+        
+      </div>
+      <div className="gpt3__header-image">
+          <img src={ai} alt="ai"/>
+      </div>
     </div>
   )
 }
